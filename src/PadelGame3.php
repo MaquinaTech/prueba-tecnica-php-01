3bc08de89f35ca20738b504fea0c4e9863062aeb
nicolopezdelerma <nicolopezdelerma@gmail.com>
2023-06-01 19:55:06 +0200

    rename variables and fix constructor

--- src/PadelGame3.php
+++ src/PadelGame3.php
@@ -6,36 +6,47 @@ namespace Sivsa\PadelGame;
 
 class PadelGame3 implements PadelGame
 {
-    private int $p2 = 0;
+    //Score of the players
+    private int $scorePlayer1 = 0;
 
-    private int $p1 = 0;
+    private int $scorePlayer2 = 0;
 
-    public function __construct(
-        private string $p1N,
-        private string $p2N
-    ) {
+    //Names of the players
+    private string $player1;
+
+    private string $player2;
+
+    /**
+     * PadelGame1 constructor.
+     * @parameters string $player1
+     * @parameters string $player2
+     */
+    public function __construct(string $player1, string $player2)
+    {
+        $this->player1 = $player1;
+        $this->player2 = $player2;
     }
 
     public function getScore(): string
     {
-        if ($this->p1 < 4 && $this->p2 < 4 && ! ($this->p1 + $this->p2 === 6)) {
+        if ($this->scorePlayer1 < 4 && $this->scorePlayer2 < 4 && ! ($this->scorePlayer1 + $this->scorePlayer2 === 6)) {
             $p = ['Love', 'Fifteen', 'Thirty', 'Forty'];
-            $s = $p[$this->p1];
-            return ($this->p1 === $this->p2) ? "{$s}-All" : "{$s}-{$p[$this->p2]}";
+            $s = $p[$this->scorePlayer1];
+            return ($this->scorePlayer1 === $this->scorePlayer2) ? "{$s}-All" : "{$s}-{$p[$this->scorePlayer2]}";
         }
-        if ($this->p1 === $this->p2) {
+        if ($this->scorePlayer1 === $this->scorePlayer2) {
             return 'Deuce';
         }
-        $s = $this->p1 > $this->p2 ? $this->p1N : $this->p2N;
-        return (($this->p1 - $this->p2) * ($this->p1 - $this->p2) === 1) ? "Advantage {$s}" : "Win for {$s}";
+        $s = $this->scorePlayer1 > $this->scorePlayer2 ? $this->player1 : $this->player2;
+        return (($this->scorePlayer1 - $this->scorePlayer2) * ($this->scorePlayer1 - $this->scorePlayer2) === 1) ? "Advantage {$s}" : "Win for {$s}";
     }
 
     public function wonPoint(string $playerName): void
     {
         if ($playerName === 'player1') {
-            $this->p1++;
+            $this->scorePlayer1++;
         } else {
-            $this->p2++;
+            $this->scorePlayer2++;
         }
     }
 }
